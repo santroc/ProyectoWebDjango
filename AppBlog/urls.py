e111307634b39e81f5db01d00fd273254d4df929
Santiago Troitiño Cristancho <santroc.adm@gmail.com>
2022-09-16 15:32:39 -0500

    Added forms and views for all available models

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from AppBlog.views import *
 
 urlpatterns = [
-    path('', inicio, name='Inicio'),
-    path('addEstudiante', estudianteFormulario, name='addEstudiante')
+    path('home', inicio, name='Inicio'),
+    path('addEstudiante', estudianteFormulario, name='addEstudiante'),
+    path('addCurso', cursoFormulario, name='cursoEstudiante'),
+    path('addProfesor', profesorFormulario, name='cursoProfesor'),
+    path('addEntregable', entregableFormulario, name='cursoEntregable'),
 ]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -16,9 +16,48 @@ def estudianteFormulario(request):
     
         estudiante = Estudiante(nombre = request.POST['nombre'], apellido = request.POST['apellido'], email = request.POST['email'])
         estudiante.save()
-        #return messages.success(request, 'Estudiante agregado con éxito')
-        return render(request, 'padre.html')
+        messages.success(request, '¡Estudiante agregado con éxito!')
+        return render(request, 'forms/estudiante.html')
 
 
 
     return render(request, 'forms/estudiante.html')
+
+
+def cursoFormulario(request):
+
+    if(request.method == 'POST'):
+
+        curso = Curso(nombre = request.POST['nombre'], camada = request.POST['camada'])
+        curso.save()
+        messages.success(request, '¡Curso agregado con éxito!')
+        return render(request, 'forms/curso.html')
+
+
+    return render(request, 'forms/curso.html')
+
+def entregableFormulario(request):
+
+    if(request.method == 'POST'):
+        if(request.POST.get('enable') == None):
+            entregable = Entregable(nombre = request.POST['nombre'], fechaDeEntrega = request.POST['fecha'], entregado = False)
+        else:
+            entregable = Entregable(nombre = request.POST['nombre'], fechaDeEntrega = request.POST['fecha'], entregado = request.POST['entregado'])
+        entregable.save()
+        messages.success(request, '¡Entregable agregado con éxito!')
+        return render(request, 'forms/entregable.html')
+
+
+    return render(request, 'forms/entregable.html')
+
+def profesorFormulario(request):
+
+    if(request.method == 'POST'):
+
+        profesor = Profesor(nombre = request.POST['nombre'], apellido = request.POST['apellido'], email = request.POST['email'], profesion = request.POST['profesion'])
+        profesor.save()
+        messages.success(request, '¡Profesor agregado con éxito!')
+        return render(request, 'forms/profesor.html')
+
+
+    return render(request, 'forms/profesor.html')
