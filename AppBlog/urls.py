06edd06185eab8094a3761febd373f8c291a7c95
Santiago Troitiño Cristancho <santroc.adm@gmail.com>
2022-09-17 14:54:50 -0500

    Fixed project files

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -3,8 +3,6 @@ from AppBlog.views import *
 
 urlpatterns = [
     path('home', inicio, name='Inicio'),
-    path('addEstudiante', estudianteFormulario, name='addEstudiante'),
     path('addCurso', cursoFormulario, name='cursoEstudiante'),
     path('addProfesor', profesorFormulario, name='cursoProfesor'),
-    path('addEntregable', entregableFormulario, name='cursoEntregable'),
 ]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -10,20 +10,6 @@ def inicio(request):
     return render(request, 'padre.html')
 
 
-def estudianteFormulario(request):
-
-    if(request.method == 'POST'):
-    
-        estudiante = Estudiante(nombre = request.POST['nombre'], apellido = request.POST['apellido'], email = request.POST['email'])
-        estudiante.save()
-        messages.success(request, '¡Estudiante agregado con éxito!')
-        return render(request, 'forms/estudiante.html')
-
-
-
-    return render(request, 'forms/estudiante.html')
-
-
 def cursoFormulario(request):
 
     if(request.method == 'POST'):
@@ -35,20 +21,6 @@ def cursoFormulario(request):
 
 
     return render(request, 'forms/curso.html')
-
-def entregableFormulario(request):
-
-    if(request.method == 'POST'):
-        if(request.POST.get('enable') == None):
-            entregable = Entregable(nombre = request.POST['nombre'], fechaDeEntrega = request.POST['fecha'], entregado = False)
-        else:
-            entregable = Entregable(nombre = request.POST['nombre'], fechaDeEntrega = request.POST['fecha'], entregado = request.POST['entregado'])
-        entregable.save()
-        messages.success(request, '¡Entregable agregado con éxito!')
-        return render(request, 'forms/entregable.html')
-
-
-    return render(request, 'forms/entregable.html')
 
 def profesorFormulario(request):
 
