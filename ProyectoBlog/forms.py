35c7328716a27c67200e1b0f0bc712578c99e85d
Santiago Troitiño Cristancho <santroc.adm@gmail.com>
2022-10-21 09:45:47 -0500

    Removed broken reference

--- ProyectoBlog/forms.py
+++ ProyectoBlog/forms.py
@@ -1,7 +1,5 @@
 from .models import *
 from email import contentmanager
-from tkinter import Widget
-from turtle import title
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
